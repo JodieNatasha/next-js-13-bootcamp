93857020edb15c7d8058cb1a830910f35581b77b
Jodie Rix <jodierix@Mac-Mini.local>
2023-05-16 12:58:48 +0100

    added tailwind

--- .next/server/flight-server-css-manifest.js
+++ .next/server/flight-server-css-manifest.js
@@ -1,3 +1,15 @@
 self.__RSC_CSS_MANIFEST={
-  "__entry_css_mods__": {}
+  "__entry_css_mods__": {
+    "/Users/jodierix/Desktop/next-js-13-bootcamp/app/page": [
+      "/Users/jodierix/Desktop/next-js-13-bootcamp/app/page.module.css",
+      "/Users/jodierix/Desktop/next-js-13-bootcamp/app/globals.css"
+    ]
+  },
+  "/Users/jodierix/Desktop/next-js-13-bootcamp/app/layout.tsx": [
+    "/Users/jodierix/Desktop/next-js-13-bootcamp/app/globals.css"
+  ],
+  "/Users/jodierix/Desktop/next-js-13-bootcamp/app/page.tsx": [
+    "/Users/jodierix/Desktop/next-js-13-bootcamp/app/page.module.css",
+    "/Users/jodierix/Desktop/next-js-13-bootcamp/node_modules/@next/font/google/target.css?{\"path\":\"app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+  ]
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className={styles.main}>
-     <h1>Hello</h1>
+     <h1 className="text-red-700 text-7xl">Hello</h1>
     </main>
   )
 }
